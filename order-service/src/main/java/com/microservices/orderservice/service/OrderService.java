650ac6097668ea3478e5b3de323219a6a8a81ad2
Rishi <Rishijha3033@gmail.com>
2023-11-23 06:08:36 +0530

    Interservice communication using web client module order service communicating with the inventory service to check stock

--- order-service/src/main/java/com/microservices/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/microservices/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.microservices.orderservice.service;
 
+import com.microservices.orderservice.dto.InventoryResponse;
 import com.microservices.orderservice.dto.OrderLineItemDto;
 import com.microservices.orderservice.dto.OrderRequest;
 import com.microservices.orderservice.model.OrderLineItem;
@@ -7,7 +8,9 @@ import com.microservices.orderservice.model.Order;
 import com.microservices.orderservice.repository.OrderRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -16,9 +19,11 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
 
-    public OrderService(OrderRepository orderRepository) {
+    public OrderService(OrderRepository orderRepository, WebClient webClient) {
         this.orderRepository = orderRepository;
+        this.webClient = webClient;
     }
 
     public void placeOrder(OrderRequest orderRequest){
@@ -29,8 +34,22 @@ public class OrderService {
                 .map(this::mapToDto).toList();
 
         order.setOrderLineItems(orderLineItemList);
+        List<String> skuCodes = order.getOrderLineItems().stream()
+                                .map(OrderLineItem::getSkuCode).toList();
+        //Getting the skuCodes of all the orders into a list to pass it to the inventory service
 
-        orderRepository.save(order);
+        // Call Inventory service and place order if product is in stock
+        InventoryResponse[] inventoryResponseArray = webClient.get()
+                                    .uri("http://localhost:8082/api/inventory",
+                                            uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())//passing the skucodes to inventory service using the webclient
+                                    .retrieve()
+                                    .bodyToMono(InventoryResponse[].class)
+                                    .block();
+        assert inventoryResponseArray != null;
+        boolean allProductInStock = Arrays.stream(inventoryResponseArray).allMatch(InventoryResponse::isInStock);
+        if (allProductInStock){orderRepository.save(order);}
+        else {throw  new IllegalArgumentException("Product is not in stock please try again later");}
+
     }
 
     private OrderLineItem mapToDto(OrderLineItemDto orderLineItemDto) {
