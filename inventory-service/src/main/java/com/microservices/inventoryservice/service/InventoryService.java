b9f0a82df4fea0e94d8c1a5074e4b6da2a07ca74
Rishi <Rishijha3033@gmail.com>
2023-11-30 06:22:33 +0530

    Added Circuit_Breaker, Timeout and Retry functionality for order service using resilinece4j

--- inventory-service/src/main/java/com/microservices/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/microservices/inventoryservice/service/InventoryService.java
@@ -3,6 +3,7 @@ package com.microservices.inventoryservice.service;
 import com.microservices.inventoryservice.dto.InventoryResponse;
 import com.microservices.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,12 +11,16 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
-    public List<InventoryResponse> isInStock(List<String> skuCode)
-    {
+    public List<InventoryResponse> isInStock(List<String> skuCode){
+        //stimulating the wait behaviour to test the timeout functionality
+//        log.info("Wait Started");
+//        Thread.sleep(10000);
+//        log.info("Wait Ended");
         return inventoryRepository.findBySkuCodeIn(skuCode).stream()
                 .map(inventory ->
                     InventoryResponse.builder()
